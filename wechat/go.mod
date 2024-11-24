c8dad90d9c8243383c8441bedc5537f8d2c83f97
huangqian <huangqian1985@qq.com>
2024-11-24 10:13:28 +0800

    商圈通知结构定义

--- wechat/go.mod
+++ wechat/go.mod
@@ -22,30 +22,26 @@ require (
 	github.com/bradfitz/gomemcache v0.0.0-20220106215444-fb4bf637b56d // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cloudflare/circl v1.4.0 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/google/pprof v0.0.0-20240910150728-a0b0bb1d4134 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/onsi/ginkgo/v2 v2.20.2 // indirect
 	github.com/quic-go/qpack v0.5.1 // indirect
 	github.com/quic-go/quic-go v0.47.0 // indirect
 	github.com/refraction-networking/utls v1.6.7 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/tidwall/gjson v1.14.1 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
-	github.com/urfave/cli/v2 v2.27.5 // indirect
-	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	go.uber.org/mock v0.4.0 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
@@ -55,5 +51,4 @@ require (
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 	golang.org/x/tools v0.25.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ wechat/pkg/data/business_circle.go
@@ -0,0 +1,92 @@
+package data
+
+type BusinessCircleNotifyBase struct {
+	Id           string `json:"id"`            // 通知的唯一ID
+	CreateTime   string `json:"create_time"`   // 通知时间(遵循rfc3339标准格式)
+	EventType    string `json:"event_type"`    // 通知的类型， 会员积分服务授权通知的类型为：MALL_AUTH.ACTIVATE_CARD
+	ResourceType string `json:"resource_type"` // 通知的资源数据类型
+	Summary      string `json:"summary"`       // 回调摘要
+}
+
+// 微信通知数据结构
+type OriginBusinessCircleNotifySource struct {
+	Algorithm      string `json:"algorithm"`       // 对支付结果数据进行加密的加密算法
+	Ciphertext     string `json:"ciphertext"`      // Base64编码后的支付结果数据密文
+	OriginalType   string `json:"original_type"`   // 原始回调类型为：discount_card
+	AssociatedData string `json:"associated_Data"` // 附加数据
+	Nonce          string `json:"nonce"`           // 加密使用的随机串
+}
+
+type OriginBusinessCircleAuthor struct {
+	BusinessCircleNotifyBase
+	Source OriginBusinessCircleNotifySource `json:"source"`
+}
+
+/*
+商圈会员积分服务授权结果
+*/
+
+type BusinessCircleAuthorType string
+
+var (
+	// 会员开卡(进卡包) + 未授权会员积分服务
+	BusinessCircleAuthorType_Reg BusinessCircleAuthorType = "REGISTERED_MODE"
+	// 会员开卡(进卡包）+ 授权会员积分服务
+	BusinessCircleAuthorType_RegAndAuth BusinessCircleAuthorType = "REGISTERED_AND_AUTHORIZATION_MODE"
+)
+
+type BusinessCircleAuthorSource struct {
+	Openid   string                   `json:"openid"`    // 顾客授权时使用的小程序上的OpenID
+	Code     string                   `json:"code"`      // 用户开会员卡时的商圈商户号
+	Mchid    string                   `json:"mchid"`     // 用户开会员卡时的商圈商户号
+	AuthType BusinessCircleAuthorType `json:"auth_type"` // 用户授权类型
+}
+
+type BusinessCircleAuthor struct {
+	BusinessCircleNotifyBase
+	Source BusinessCircleAuthorSource `json:"source"`
+}
+
+type BusinessCircleShopBase struct {
+	Mchid        string `json:"mchid"`         // 微信支付分配的商户号
+	MerchantName string `json:"merchant_name"` // 商圈商户名称
+	ShopName     string `json:"shop_name"`     // 门店名称，商圈在商圈小程序上圈店时填写的门店名称
+	ShopNumber   string `json:"shop_number"`   // 门店编号，商圈在商圈小程序上圈店时填写的门店编号，用于跟商圈自身已有的商户识别码对齐
+}
+
+/*
+商圈会员场内支付结果
+*/
+type BusinessCirclePaymentSource struct {
+	BusinessCircleShopBase
+	Appid         string `json:"appid"`          // 顾客授权积分时使用的小程序的AppID
+	Openid        string `json:"openid"`         // 顾客授权时使用的小程序上的OpenID
+	TimeEnd       string `json:"time_end"`       // 交易完成时间，遵循[rfc3339]标准格式
+	Amount        int    `json:"amount"`         // 用户实际消费金额，单位（分）
+	TransactionId string `json:"transaction_id"` // 微信支付订单号
+	CommitTag     string `json:"commit_tag"`     // 手动提交积分标记，自动提交时无该字段，用于区分用户手动申请后推送的积分数据
+}
+
+type BusinessCirclePayment struct {
+	BusinessCircleNotifyBase
+	Source BusinessCirclePaymentSource `json:"source"`
+}
+
+/*
+商圈会员场内退款结果
+*/
+type BusinessCircleRefundSource struct {
+	BusinessCircleShopBase
+	Appid         string `json:"appid"`          // 顾客授权积分时使用的小程序的AppID
+	Openid        string `json:"openid"`         // 顾客授权时使用的小程序上的OpenID
+	RefundTime    string `json:"time_end"`       // 交易完成时间，遵循[rfc3339]标准格式
+	PayAmount     int    `json:"pay_amount"`     // 用户实际消费金额，单位（分）
+	RefundAmount  int    `json:"refund_amount"`  // 用户退款金额，单位（分）
+	TransactionId string `json:"transaction_id"` // 微信支付订单号
+	RefundId      string `json:"refund_id"`      // 微信支付退款单号
+}
+
+type BusinessCircleRefund struct {
+	BusinessCircleNotifyBase
+	Source BusinessCircleRefundSource `json:"source"`
+}
