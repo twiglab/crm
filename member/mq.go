692fa5287d13a9ec7fa0c178c8e9c91b8821d82c
it512 <it512@msn.com>
2024-12-10 21:21:30 +0800

    mq

---
+++ member/mq.go
@@ -0,0 +1,82 @@
+package member
+
+import (
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitMQ struct {
+	conn    *amqp.Connection
+	channel *amqp.Channel
+	//队列名称
+	QueueName string
+	//交换机名称
+	Exchange string
+	//bind Key 名称
+	Key string
+	//连接信息
+	Mqurl string
+}
+
+// 话题模式接受消息
+// 要注意key,规则
+// 其中“*”用于匹配一个单词，“#”用于匹配多个单词（可以是零个）
+// 匹配 kuteng.* 表示匹配 kuteng.hello, kuteng.hello.one需要用kuteng.#才能匹配到
+func (r *RabbitMQ) RecieveTopic() {
+	//1.试探性创建交换机
+	err := r.channel.ExchangeDeclare(
+		r.Exchange,
+		//交换机类型
+		amqp.ExchangeTopic,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	r.failOnErr(err, "Failed to declare an exch"+
+		"ange")
+	//2.试探性创建队列，这里注意队列名称不要写
+	q, err := r.channel.QueueDeclare(
+		"", //随机生产队列名称
+		false,
+		false,
+		true,
+		false,
+		nil,
+	)
+	r.failOnErr(err, "Failed to declare a queue")
+
+	//绑定队列到 exchange 中
+	err = r.channel.QueueBind(
+		q.Name,
+		//在pub/sub模式下，这里的key要为空
+		r.Key,
+		r.Exchange,
+		false,
+		nil)
+
+	//消费消息
+	messges, err := r.channel.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range messges {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	fmt.Println("退出请按 CTRL+C\n")
+	<-forever
+}
