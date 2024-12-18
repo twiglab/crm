7fd57d54b096a0dbf1ad1ead6d4bdc1efa2152e0
it512 <it512@msn.com>
2024-12-18 15:07:16 +0800

    ext name

--- wechat/pkg/bc/mq.go
+++ wechat/pkg/bc/mq.go
@@ -1,6 +1,6 @@
 package bc
 
-const MQ_BC_EXCHANGE_NAME = "exchange.bc"
+const MQ_BC_EXCHANGE_NAME = "ext.wx.bc"
 const MQ_WX_TOC_BC_AUTH = "wx.bc.auth"
 const MQ_WX_TOC_BC_PAYMENT = "wx.bc.payment"
 const MQ_WX_TOC_BC_REFUND = "wx.bc.refund"
