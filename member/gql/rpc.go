b47d64e436edd96da9fe136d2197c2aebae7bea1
it512 <it512@msn.com>
2024-12-28 15:09:27 +0800

    member fb

---
+++ member/gql/rpc.go
@@ -0,0 +1,17 @@
+package gql
+
+import (
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi/v5"
+	"github.com/twiglab/crm/member/orm/ent"
+	"github.com/twiglab/crm/member/rpc/gql/graph"
+	"github.com/twiglab/crm/psdk/gqlx"
+)
+
+func New(client *ent.Client) chi.Router {
+	srv := gqlx.NewGQLHandler(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{Client: client}}))
+	r := chi.NewRouter()
+	r.Handle("/", playground.ApolloSandboxHandler("pg", "/gql/query"))
+	r.Handle("/query", srv)
+	return r
+}
