6d68f59cde310810d9e94077610bfef6fc2550c6
it512 <it512@msn.com>
2024-12-30 23:12:08 +0800

    fix

--- member/cmd/proxy/main.go
+++ member/cmd/proxy/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/twiglab/crm/member/mq"
 	"github.com/twiglab/crm/member/orm"
+	"github.com/twiglab/crm/psdk/quit"
 	"github.com/twiglab/crm/wechat/pkg/bc"
 )
 
@@ -18,8 +18,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(db.Ping())
-
 	client := orm.OpenClient(db)
 
 	conn, err := mq.Dial("amqp://erp:erpPass123@pipi.dev:5672/")
@@ -28,19 +26,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	recvExitChan := make(chan struct{})
-
 	q := &mq.RabbitMQ{
 		Conn:      conn,
 		QueueName: "q.member.auth",
 		Exchange:  bc.MQ_BC_EXCHANGE_NAME,
 		BindKey:   bc.MQ_WX_TOC_BC_AUTH,
-		ExitChan:  recvExitChan,
 	}
 
-	if err := q.Recieve(context.Background(), &mq.MemberAuthHandle{Client: client}); err != nil {
+	ctx, cancelFn := context.WithCancel(context.Background())
+	defer cancelFn()
+
+	if err := q.Recieve(ctx, &mq.MemberAuthHandle{Client: client}); err != nil {
 		log.Fatal(err)
 	}
 
-	select {}
+	quit.WaitSignal()
 }
