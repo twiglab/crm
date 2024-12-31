251eee7c59df732522bfa7582766b2ac9d4e87b0
it512 <it512@msn.com>
2024-12-31 08:58:17 +0800

    testbed

---
+++ wechat/testbed/gql/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/twiglab/crm/wechat/mq"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct{
+	Q *mq.XMQ
+}
